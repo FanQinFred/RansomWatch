40ca6f615324f8486053f9c220a309d5f5752e7d
FanQinFred <56213127+FanQinFred@users.noreply.github.com>
2021-10-28 10:29:13 +0800

    Update MyForm.cpp

--- Application/MyForm.cpp
+++ Application/MyForm.cpp
@@ -14,7 +14,7 @@ HRESULT AntiRansomWareApp::MyForm::initWorkThread()
 	for (DWORD i = 0; i < numOfThreads; i++)
 	{
 		threads[i] = CreateThread(NULL,
-								  0,
+								  10000,
 								  (LPTHREAD_START_ROUTINE)FilterWorker,
 								  &context,
 								  0,
